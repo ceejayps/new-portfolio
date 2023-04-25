e87ca0b879c771e6fd4dc676f4662bd68af56cad
ceejayps <ceejayps1308@gmail.com>
2023-04-25 17:38:23 -0500

    Create hero.jsx

---
+++ src/components/hero.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import Nav from "./nav";
+import SocialMedia from "./SocialMedia_header";
+import Category from "./Category_header";
+import Images_header from "./Images_header";
+import HeaderText from "./HeaderText";
+
+const Hero = () => {
+  return (
+    <div className="bg-white pb-6 sm:pb-8 lg:pb-12">
+      <header className="mb-8 border-b">
+        <Nav />
+      </header>
+
+      <section className="mx-auto pt-8 lg:pt-16 max-w-screen-2xl px-4 md:px-8">
+        <div className="mb-8 flex h-[72vh] flex-wrap justify-between md:mb-16">
+          <HeaderText/>
+          <Images_header />
+        </div>
+        <div className="flex flex-col items-center justify-between gap-8 md:flex-row">
+          <Category />
+          <SocialMedia />
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Hero;
