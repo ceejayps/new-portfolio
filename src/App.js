399f08e4ddbbcae6b9072fc58580238159ed9c99
ceejayps <ceejayps1308@gmail.com>
2023-04-25 17:42:31 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Hero from './components/hero';
 import SkillSection from './components/SkillSection';
+import WorkSection from './components/WorkSection';
 
 function App() {
   return (
     <div className="App">
       <Hero/>
       <SkillSection/>
+      <WorkSection/>
     </div>
   );
 }
