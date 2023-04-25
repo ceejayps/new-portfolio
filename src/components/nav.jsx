955f4ac113d15dc3c0eb4115750cb6f781289d65
ceejayps <ceejayps1308@gmail.com>
2023-04-25 17:38:29 -0500

    Create nav.jsx

---
+++ src/components/nav.jsx
@@ -0,0 +1,27 @@
+import React from 'react'
+
+const Nav = () => {
+  return (
+    <div className=' fixed top-0   px-10 flex h-16 bg-slate-600 w-screen justify-between items-center text-slate-300'>
+        <div>
+            <h1>LOGO</h1>
+        </div>
+        <div className='lg:flex gap-10 items-center hidden'>
+            <ul className='flex gap-5'>
+                <li>About</li>
+                <li>Skills</li>
+                <li>Portfolio</li>
+                <li>Work History</li>
+                <li>Contact</li>
+            </ul>
+
+            <div className=' rounded-3xl w-40 h-12 bg-slate-300 flex justify-center items-center text-slate-600'>
+                <p className=''>Hire</p>
+            </div>
+        </div>
+
+    </div>
+  )
+}
+
+export default Nav
