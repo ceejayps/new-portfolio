81e00d78ad569b4f7cc6802d1e0bcbe686347f53
ceejayps <ceejayps1308@gmail.com>
2023-04-26 14:42:23 -0500

    Update WorkExpierence.jsx

--- src/components/WorkExpierence.jsx
+++ src/components/WorkExpierence.jsx
@@ -19,10 +19,10 @@ const WorkExpierence = () => {
             <li className="mb-10 ml-4">
               <div className="absolute w-3 h-3 bg-gray-200 rounded-full mt-1.5 -left-1.5 border border-white dark:border-gray-900 dark:bg-gray-700"></div>
               <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
-                February 2022
+                June 2022 - January 2023
               </time>
-              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                Application UI code in Tailwind CSS
+              <h3 className="text-lg font-semibold  dark:text-gray-900">
+              vertis Tech 
               </h3>
               <p className="mb-4 text-base font-normal text-gray-500 dark:text-gray-400">
                 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Sequi, ullam ut. Assumenda, debitis illum repellendus eligendi totam aliquam aut rerum, facere ducimus, deleniti laudantium repudiandae sit quasi consequuntur possimus atque!
@@ -51,7 +51,7 @@ const WorkExpierence = () => {
               <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                 March 2022
               </time>
-              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-gray-900">
                 Marketing UI design in Figma
               </h3>
               <p className="text-base font-normal text-gray-500 dark:text-gray-400">
@@ -63,7 +63,7 @@ const WorkExpierence = () => {
               <time className="mb-1 text-sm font-normal leading-none text-gray-400 dark:text-gray-500">
                 April 2022
               </time>
-              <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
+              <h3 className="text-lg font-semibold text-gray-900 dark:text-gray-900">
                 E-Commerce UI code in Tailwind CSS
               </h3>
               <p className="text-base font-normal text-gray-500 dark:text-gray-400">
