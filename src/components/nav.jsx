543c42cc60c08094eb962479af502ed8d33c7f51
ceejayps <ceejayps1308@gmail.com>
2023-04-25 21:24:24 -0500

    Update nav.jsx

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Nav = () => {
   return (
-    <div className=' fixed top-0  z-[99999] backdrop-blur-2xl px-10 flex h-16 bg-white/80 w-screen justify-between items-center text-slate-900'>
+    <div className=' fixed top-0  z-[99999] backdrop-blur-2xl px-10 flex h-16 bg-white/70 w-screen justify-between items-center text-slate-900'>
         <div>
             <h1>LOGO</h1>
         </div>
