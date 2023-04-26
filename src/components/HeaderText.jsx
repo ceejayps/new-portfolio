52f60ec3b69c02ff3e1f3f7f7868d97fa2d74ed2
ceejayps <ceejayps1308@gmail.com>
2023-04-25 21:24:33 -0500

    Update HeaderText.jsx

--- src/components/HeaderText.jsx
+++ src/components/HeaderText.jsx
@@ -1,34 +1,36 @@
 import { useState, useEffect } from 'react';
 
 
-const TextTypingEffect = ({ texts }) => {
+const TextTypingEffect = (props) => {
   const [displayText, setDisplayText] = useState('');
   const [currentTextIndex, setCurrentTextIndex] = useState(0);
 
   useEffect(() => {
     let currentIndex = 0;
     const intervalId = setInterval(() => {
-      if (currentIndex <= texts[currentTextIndex].length) {
-        setDisplayText(texts[currentTextIndex].slice(0, currentIndex));
+      if (currentIndex <= props.texts[currentTextIndex].length) {
+        setDisplayText(props.texts[currentTextIndex].slice(0, currentIndex));
         currentIndex++;
       } else {
         clearInterval(intervalId);
         setTimeout(() => {
-          setCurrentTextIndex((currentTextIndex + 1) % texts.length);
-        }, 5000);
+          setCurrentTextIndex((currentTextIndex + 1) % props.texts.length);
+          props.onChildData((currentTextIndex + 1) % props.texts.length)
+        }, 2000);
       }
     }, 100);
+    
     return () => clearInterval(intervalId);
-  }, [currentTextIndex, texts]);
+  }, [currentTextIndex, props.texts]);
 
   return (
     
-      <p>{displayText} Engineer</p>
+      <p>{displayText } Engineer</p>
    
   );
 };
 
-const HeaderText = () => {
+const HeaderText = ({onChildData}) => {
 
     const texts = [
         "Software",
@@ -40,11 +42,11 @@ const HeaderText = () => {
 
   return (
     <div className="mb-6 flex w-full flex-col justify-center sm:mb-12 lg:mb-0 lg:w-1/3 lg:pt-48 lg:pb-24">
-    <h1 className="text-black-800 mb-1 text-5xl font-bold sm:text-5xl md:mb-2 md:text-5xl">
+    <h1 className="text-black-800 mb-1 text-2xl font-bold sm:text-5xl md:mb-2 md:text-5xl">
       Chester Johnson
     </h1>
     <p className="text-black-800 mb-4 text-xl font-bold sm:text-xl md:mb-8 md:text-2xl">
-    <TextTypingEffect  texts={texts} /> 
+    <TextTypingEffect  texts={texts} onChildData={onChildData} /> 
     </p>
 
    
