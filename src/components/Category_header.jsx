be5a085521cc77cd9ea2be8801ced54bc4684c86
ceejayps <ceejayps1308@gmail.com>
2023-04-26 07:52:13 -0500

    Update Category_header.jsx

--- src/components/Category_header.jsx
+++ src/components/Category_header.jsx
@@ -3,10 +3,10 @@ import React from 'react'
 const Category = () => {
   return (
     <div className="flex h-12 w-72 divide-x overflow-hidden rounded-lg border">
-    <a href="#" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Web</a>
-    <a href="#" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Apps</a>
-    <a href="#" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Games</a>
-    <a href="#" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Servers</a>
+    <a href="/web" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Web</a>
+    <a href="/apps" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Apps</a>
+    <a href="/Games" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Games</a>
+    <a href="/servers" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Servers</a>
   </div>
   )
 }
