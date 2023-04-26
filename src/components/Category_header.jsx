24d813089a5ef9790fa6a4c5f8382632564a84e1
ceejayps <ceejayps1308@gmail.com>
2023-04-26 10:55:35 -0500

    Update Category_header.jsx

--- src/components/Category_header.jsx
+++ src/components/Category_header.jsx
@@ -2,12 +2,15 @@ import React from 'react'
 
 const Category = () => {
   return (
-    <div className="flex h-12 w-72 divide-x overflow-hidden rounded-lg border">
-    <a href="/web" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Web</a>
+    <div>
+    <div className=" hidden flex h-12 w-72 divide-x overflow-hidden rounded-lg border">
+        
+    <a href="?type=Portfolio" onClick={(e)=> e.preventDefault()} className=" flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Web</a>
     <a href="/apps" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Apps</a>
     <a href="/Games" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Games</a>
     <a href="/servers" className="flex w-1/3 items-center justify-center text-gray-500 transition duration-100 hover:bg-gray-100 active:bg-gray-200">Servers</a>
-  </div>
+    </div>
+    </div>
   )
 }
 
