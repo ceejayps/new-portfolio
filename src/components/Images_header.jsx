20a149c9a7a1f2ad82d7dd9f8e0416ee03d8e75c
ceejayps <ceejayps1308@gmail.com>
2023-04-25 21:24:26 -0500

    Update Images_header.jsx

--- src/components/Images_header.jsx
+++ src/components/Images_header.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from "react";
 
 
-const Images_header = () => {
+const Images_header = ({index}) => {
     const images = [
         'https://images.unsplash.com/photo-1481487196290-c152efe083f5?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1924&q=80',
         'https://images.unsplash.com/photo-1467232004584-a241de8bcf5d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1769&q=80',
@@ -12,27 +12,12 @@ const Images_header = () => {
         'https://images.unsplash.com/photo-1551288049-bebda4e38f71?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1770&q=80'
 
     ]
-
-
-
-    const [currentImageIndex, setCurrentImageIndex] = useState(0);
-
-
-
-  useEffect(() => {
-    const interval = setInterval(() => {
-      setCurrentImageIndex((currentImageIndex + 1) % images.length);
-    }, 5000);
-    return () => clearInterval(interval);
-  }, [currentImageIndex, images.length]);
-
-
   return (
     <div className="mb-12 flex  md:mb-16 lg:w-[55%]">
     
 
     <div className="overflow-hidden rounded-lg bg-gray-100 shadow-lg">
-      <img src={images[currentImageIndex]} loading="lazy" alt="Photo by Manny Moreno" className="h-full w-full object-cover object-center" />
+      <img src={images[index]} loading="lazy" alt="Photo by Manny Moreno" className="h-full w-full object-cover object-center" />
     </div>
   </div>
   )
