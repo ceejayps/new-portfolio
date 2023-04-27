a3bc6526624adc0617e9ae850bc12a3426c0a6ce
ceejayps <ceejayps1308@gmail.com>
2023-04-27 06:16:36 -0500

    Update nav.jsx

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,19 +4,19 @@ const Nav = () => {
   return (
     <div className=' fixed top-0  z-[99999] backdrop-blur-2xl px-10 flex h-16 bg-white/70 w-screen justify-between items-center text-slate-900'>
         <div>
-            <h1>LOGO</h1>
+            <h1 className='text-2xl font-bold'>CEEJAYPS</h1>
         </div>
         <div className='lg:flex gap-10 items-center hidden'>
-            <ul className='flex gap-5'>
-                <li>About</li>
-                <li>Skills</li>
-                <li>Portfolio</li>
-                <li>Work History</li>
-                <li>Contact</li>
+            <ul className='flex gap-5 items-center '>
+                <li className=' cursor-pointer hover:text-slate-50 hover:bg-slate-900 py-1 px-4 rounded-md transition-all duration-500'>About</li>
+                <li className=' cursor-pointer hover:text-slate-50 hover:bg-slate-900 py-1 px-4 rounded-md transition-all duration-500'>Skills</li>
+                <li className=' cursor-pointer hover:text-slate-50 hover:bg-slate-900 py-1 px-4 rounded-md transition-all duration-500'>Portfolio</li>
+                <li className=' cursor-pointer hover:text-slate-50 hover:bg-slate-900 py-1 px-4 rounded-md transition-all duration-500'>Work History</li>
+                <li className=' cursor-pointer hover:text-slate-50 hover:bg-slate-900 py-1 px-4 rounded-md transition-all duration-500'>Contact</li>
             </ul>
 
-            <div className=' rounded-3xl w-40 h-12 bg-slate-300 flex justify-center items-center text-slate-600'>
-                <p className=''>Hire</p>
+            <div className=' cursor-pointer rounded-3xl w-40 h-12 bg-slate-900 flex justify-center items-center text-slate-300 duration-200 bg-black border-2 border-black rounded-full inline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none focus-visible:outline-black text-sm focus-visible:ring-black'>
+                <p className=' font-bold text4xl'>Hire Me</p>
             </div>
         </div>
 
