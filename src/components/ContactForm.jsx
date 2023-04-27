dfd7b2e491b2ab677d1ae78ed5230bdd2e08f3d3
ceejayps <ceejayps1308@gmail.com>
2023-04-27 06:16:38 -0500

    Update ContactForm.jsx

--- src/components/ContactForm.jsx
+++ src/components/ContactForm.jsx
@@ -49,7 +49,7 @@ const ContactForm = () => {
                 </div>
               </div>
               <div className="col-span-full">
-                <button className="items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full nline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none focus-visible:outline-black text-sm focus-visible:ring-black" type="submit">
+                <button className="items-center justify-center w-full px-6 py-2.5 text-center text-white duration-200 bg-black border-2 border-black rounded-full inline-flex hover:bg-transparent hover:border-black hover:text-black focus:outline-none focus-visible:outline-black text-sm focus-visible:ring-black" type="submit">
                   Submit your request
                 </button>
               </div>
