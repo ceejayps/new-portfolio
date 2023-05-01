f68449a2553994b22097a708228f6bc83b07208b
ceejayps <ceejayps1308@gmail.com>
2023-05-01 05:04:54 -0500

    Update WorkSection.jsx

--- src/components/WorkSection.jsx
+++ src/components/WorkSection.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const WorkSection = () => {
   return (
-    <div className="bg-white py-6 sm:py-8 lg:py-12">
+    <div id='Portfolio' className="bg-white py-6 sm:py-8 lg:py-12">
   <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
     <div className="mb-4 flex items-center justify-between gap-8 sm:mb-8 md:mb-12">
       <div className="flex items-center gap-12">
