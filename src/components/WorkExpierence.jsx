0c272f50c93087cd1a531b06d558cff1d8799af6
ceejayps <ceejayps1308@gmail.com>
2023-05-01 12:11:52 -0500

    Update WorkExpierence.jsx

--- src/components/WorkExpierence.jsx
+++ src/components/WorkExpierence.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const WorkExpierence = () => {
   return (
-    <div id="WorkHistory" className="bg-white py-6 sm:py-8 lg:py-12">
+    <div id="WorkHistory" className="bg-white py-6 sm:py-8 lg:py-12 scroll-mt-[50px]">
       <div className="mx-auto max-w-screen-2xl px-4 md:px-8">
         <div className="mb-10 md:mb-16">
           <h2 className="mb-4 text-center text-2xl font-bold text-gray-800 md:mb-6 lg:text-3xl">
